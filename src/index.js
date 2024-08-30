46409e851b253b4f2ca36768f522d82afe66386a
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-08-30 16:32:49 +0530

    multer & cloudinery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 import express from "express";
-
 const app = express();
 dotenv.config();
 connectDB()
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDNARY_NAME, 
+    api_key: process.env.CLOUDNARY_API_KEY, 
+    api_secret: process.env.CLOUDNARY_API_SECRET
+});
+
+
+const uploadImage = async (filePath) => {
+    try {
+        if(!filePath) return null;
+        //upload the file to cloudinary
+        const res = await cloudinary.uploader.upload(filePath , {
+            resource_type: 'auto',
+        })
+        // file uploaded successfully
+        console.log("file is uploaded to cloudinary",res.url);
+        return res;
+
+    } catch (error) {
+        fs.unlinkSync(filePath); // remove the locally saved temp file as upload operation got failed
+        return null;
+    }
+}

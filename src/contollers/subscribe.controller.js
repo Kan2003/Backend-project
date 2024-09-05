6b8c3a6ae58c66ca11a65d37d05a33bce2f809f0
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-09-05 19:03:15 +0530

    new files added

--- src/contollers/subscribe.controller.js
+++ src/contollers/subscribe.controller.js
@@ -8,12 +8,9 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 const subscribed = asyncHandler( async(req ,res) => {
     
     const userId = req.user._id;
-    // console.log(req.user)
     const channelName = req.params.channelName;
-    // console.log(userId, channelName)
 
     const channel = await User.findOne({ username : channelName });
-    // console.log('channel details' , channel)
 
     const channelId = channel?._id;
     // console.log('channel id ' , channelId)

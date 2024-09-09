23d9d353a703462d6e97e2a8ccf9ebd517ca8a63
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-09-09 14:59:01 +0530

    remaining models

---
+++ src/models/like.model.js
@@ -0,0 +1,25 @@
+import {mongoose , Schema, Types} from "mongoose";
+
+
+const likeSchema = new Schema({
+    commnet : {
+        type : Schema.Types.ObjectId,
+        ref : 'Comment'
+    },
+    video : {
+        type : Schema.Types.ObjectId,
+        ref : 'Video'
+    },
+    likedBy : {
+        type : Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    tweet : {
+        type : Schema.Types.ObjectId,
+        ref : 'Tweet'
+    }
+
+},{timestamps : true})
+
+
+export const Like = mongoose.model("Like", likeSchema);

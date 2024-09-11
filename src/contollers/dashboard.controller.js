3e381a2fe451251698658dbe33f35714c8585786
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-09-11 12:36:04 +0530

    dashboard controller-Likes functionality remains

--- src/contollers/dashboard.controller.js
+++ src/contollers/dashboard.controller.js
@@ -9,11 +9,13 @@ const getChannelStats = asyncHandler(async (req, res) => {
   const channelId = req.user._id;
 
   try {
-    console.log(channelId);
+    // console.log(channelId);
+
     const aboutChannel = await User.aggregate([
       {
         $match: { _id: channelId },
       },
+      // subscriber detils
       {
         $lookup: {
           from: "subscriptions",
@@ -24,7 +26,7 @@ const getChannelStats = asyncHandler(async (req, res) => {
             {
               $lookup: {
                 from: "users",
-                localField: "channel",
+                localField: "subscriber",
                 foreignField: "_id",
                 as: "subscriberDetails",
               },
@@ -38,18 +40,88 @@ const getChannelStats = asyncHandler(async (req, res) => {
             },
             {
               $project: {
-                username: 1,
                 _id: 1,
-                avatar: 1,
+                subscriber: 1,
+                createdAt: 1,
+                "subscriberDetails._id": 1,
+                "subscriberDetails.username": 1,
+                "subscriberDetails.fullname": 1,
+                "subscriberDetails.avatar": 1,
               },
             },
           ],
         },
       },
+      // channel susbscribed by user details
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "SubscribedChannels",
+          pipeline: [
+            {
+              $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channelDetails",
+              },
+            },
+            {
+              $addFields: {
+                channelDetails: {
+                  $first: "$channelDetails",
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 1,
+                channel: 1,
+                createdAt: 1,
+                "channelDetails._id": 1,
+                "channelDetails.username": 1,
+                "channelDetails.fullname": 1,
+                "channelDetails.avatar": 1,
+              },
+            },
+          ],
+        },
+      },
+      // total videos
+      {
+        $lookup: {
+          from : 'videos',
+          localField: '_id',
+          foreignField: 'owner',
+          as: 'videos',
+          pipeline : [
+            {
+              $project : {
+                _id : 1,
+                videoFile: 1,
+                thumbnail: 1,
+                tittle: 1,
+                createdAt: 1,
+                isPublished: 1,
+                views: 1,
+              }
+            }
+          ]
+        },
+      },
+      // total likes count on  comment , video & tweets -- worked on it 
       {
         $addFields: {
           subscriberCount: {
             $size: "$subscribedUsers",
+          },
+          SubscribedChannelsCount: {
+            $size: "$SubscribedChannels",
+          },
+          totalVideos: {
+            $size: "$videos",
           },
         },
       },
@@ -62,21 +134,25 @@ const getChannelStats = asyncHandler(async (req, res) => {
           fullname: 1,
           thumbnail: 1,
           watchHistory: 1,
-          subscribedUsers: {
-            $map: {
-              input: "$subscribedUsers",
-              as: "user",
-              in: {
-                _id: "$$user._id",
-                username: "$$user.subscriberDetails.username",
-                avatar: "$$user.subscriberDetails.avatar",
+          subscribedUsers: 1,
+          videos : 1,
+          likes: 1,
+          SubscribedChannels: 1,
+          SubscribedChannelsCount: 1,
+          totalVideos: 1,
+          likesOnComments: {
+            $size: {
+              $filter: {
+                input: "$likes",
+                as: "like",
+                cond: { $ne: ["$$like.comment", null] },
               },
             },
           },
+          
         },
       },
     ]);
-    console.log(aboutChannel);
     return res
       .status(200)
       .json(new ApiResponse(200, "Channel Stats", aboutChannel[0]));
@@ -84,5 +160,7 @@ const getChannelStats = asyncHandler(async (req, res) => {
     throw new ApiError(error.message, 500);
   }
 });
+
+
 
 export { getChannelStats };
